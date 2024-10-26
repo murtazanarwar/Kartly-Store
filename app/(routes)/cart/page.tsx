aed1c43655613201649e14272026063cf7bcb369
Murtaza Narwarwala <murtazanarwar@gmail.com>
2024-10-26 10:11:51 +0530

    add to cart feature created

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -4,6 +4,7 @@ import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 import CartItem from "./components/cart-item";
 import Summary from "./components/summary";
+import { Suspense } from "react";
 
 const CartPage = () => {
     const cart = useCart();
@@ -25,7 +26,9 @@ const CartPage = () => {
                             ))}
                             </ul>
                         </div>
-                        <Summary />
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Summary />
+                        </Suspense>
                     </div>
                 </div>
             </Container>
